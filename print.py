7bcc60449d5543bb3205a92440b2d301ce3a2b54
Pjxsantos <amadora2405@gmail.com>
2024-01-28 18:37:24 -0300

    Aula 3

---
+++ print.py
@@ -0,0 +1,57 @@
+# Sintaxe:
+# ptint (obijetos, argumentos)
+
+# mensagem = 'Função print()'
+# print (mensagem)
+# print ('Aula de Python')
+
+# nome = 'Paulo Jorge Xavier dos Santos'
+# print ('Pjxsantos -', nome)
+
+# nome = input ('Digite seu nome: ')
+# print ('Olá ' + nome + '! Bem-vindo au curso de Python!')
+
+# nome = input ('Digite seu nome: ')
+# msg = 'Olá ' + nome + '! Bem-vindo au curso de Python!'
+# print (msg)
+# print ('Outro texto')
+
+# print ('Imprime a mensagem e muda de linha')
+# print ('Imprime a mensagem e permanece na linha.', end='')
+# print (' Continuo na mesma linha!')
+
+# nome = 'Maria'
+# idade = 35
+# msg_fomatada = 'O nome dela é {0} e ela tem {1} anos' .format(nome, idade)
+# print (msg_fomatada)
+
+# nome = 'Maria'
+# idade = 35
+# print ('O nome dela é {0} e ela tem {1} anos' .format(nome, idade))
+
+# nome = 'Paulo'
+# peso = 65.70
+# msg = f'Olá, meu nome é {nome} e eu peso {peso} quilos.'
+# print (msg)
+
+# nome = 'Paulo'
+# peso = 65.70
+# print (f'Olá, meu nome é {nome} e eu peso {peso} quilos.')
+
+# a = 10
+# b = 5
+# res = f'A soma de {a} com {b} é igual a {a+b}'
+# print (res)
+
+# valor = 145.658965
+# print (f'O valor é: {valor}')
+
+# valor = 145.658965
+# print (f'O valor é: {valor:.2f}')
+
+# valor = 145.658965
+# print (f'O valor é: \'{valor:.2f}\'')
+
+nome = 'Paulo'
+idade = 35
+print (f'Nome: {nome}\tIdade: {idade}')
